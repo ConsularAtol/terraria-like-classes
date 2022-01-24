3f996ccfb870c6766e032368cc7797930bf6698b
ConsularAtol <dennis21806@gmail.com>
2022-01-23 21:05:36 -0500

    Added tooltip to wood armor

--- src/main/java/consular/classes/registry/ModItems.java
+++ src/main/java/consular/classes/registry/ModItems.java
@@ -1,9 +1,9 @@
 package consular.classes.registry;
 
 import consular.classes.Classes;
+import consular.classes.Armor.BaseArmor;
 import consular.classes.Materials.ArmorMaterialWood;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
 //import net.minecraft.item.ItemGroup;
@@ -17,10 +17,10 @@ public class ModItems {
 
     public static final ArmorMaterial WOOD_ARMOR = new ArmorMaterialWood();
     
-    public static final Item WOODEN_HELMET = new ArmorItem(WOOD_ARMOR, EquipmentSlot.HEAD, new Item.Settings());
-    public static final Item WOODEN_CHESTPLATE = new ArmorItem(WOOD_ARMOR, EquipmentSlot.CHEST, new Item.Settings());
-    public static final Item WOODEN_LEGGINGS = new ArmorItem(WOOD_ARMOR, EquipmentSlot.LEGS, new Item.Settings());
-    public static final Item WOODEN_BOOTS = new ArmorItem(WOOD_ARMOR, EquipmentSlot.FEET, new Item.Settings());
+    public static final Item WOODEN_HELMET = new BaseArmor(WOOD_ARMOR, EquipmentSlot.HEAD);
+    public static final Item WOODEN_CHESTPLATE = new BaseArmor(WOOD_ARMOR, EquipmentSlot.CHEST);
+    public static final Item WOODEN_LEGGINGS = new BaseArmor(WOOD_ARMOR, EquipmentSlot.LEGS);
+    public static final Item WOODEN_BOOTS = new BaseArmor(WOOD_ARMOR, EquipmentSlot.FEET);
 
     //registers every item.
     public static void registerItems(){
